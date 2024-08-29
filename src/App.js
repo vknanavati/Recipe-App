74ec7cb2a4e5fdc997dde61885101c90a1cea6a2
vknanavati <vimin.nanavati@gmail.com>
2024-08-29 16:32:11 -0400

    set foodType useState to use in API call, styled MUI components-Submit, Grid2, TextField, Typography.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import { Button, Grid2, Container, TextField, Typography} from '@mui/material';
+
 
 function App() {
+  console.log(process.env)
+
+  const [foodType, setFoodType] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("Searching city: ", foodType, process.env.REACT_APP_API_KEY, process.env.REACT_APP_ID)
+    fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_API_KEY}&cuisineType=${foodType}`, {
+    })
+    .then(response => response.json())
+    .then(data =>{
+        console.log("My recipe data: ", data);
+    })
+    .catch(error => console.error('Error:', error))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <Grid2 container justifyContent={"center"} direction={"column"} alignItems={"center"}>
+          <Typography
+            variant="h4"
+
+          >
+            Recipe Search
+          </Typography>
+          <Grid2>
+            <TextField
+              onChange={e=>setFoodType(e.target.value)}
+            >
+            </TextField>
+          </Grid2>
+          <Grid2>
+            <Button
+              type="submit"
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </Grid2>
+        </Grid2>
+      </form>
+    </Container>
   );
 }
 
