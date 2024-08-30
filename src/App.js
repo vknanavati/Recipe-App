c21a63cf3e3d6f325505957a710349a5cda69416
vknanavati <vimin.nanavati@gmail.com>
2024-08-30 15:03:49 -0400

    styled recipe grid, added ingredient list without quantities to recipe card.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {useState} from 'react'
-import { Button, Grid2, Container, TextField, Typography} from '@mui/material';
+import { Button,Container, TextField, Typography} from '@mui/material';
+import Grid from '@mui/material/Grid2';
 
 
 function App() {
   console.log(process.env)
 
   const [foodType, setFoodType] = useState("")
+  const [foodData, setFoodData] = useState("")
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -15,35 +17,48 @@ function App() {
     .then(response => response.json())
     .then(data =>{
         console.log("My recipe data: ", data);
+        setFoodData(data)
     })
     .catch(error => console.error('Error:', error))
   }
   return (
     <Container>
       <form onSubmit={handleSubmit}>
-        <Grid2 container justifyContent={"center"} direction={"column"} alignItems={"center"}>
+        <Grid container justifyContent={"center"} direction={"column"} alignItems={"center"}>
           <Typography
             variant="h4"
-
           >
             Recipe Search
           </Typography>
-          <Grid2>
+          <Grid>
             <TextField
               onChange={e=>setFoodType(e.target.value)}
             >
             </TextField>
-          </Grid2>
-          <Grid2>
+          </Grid>
+          <Grid>
             <Button
               type="submit"
               variant="contained"
             >
               Submit
             </Button>
-          </Grid2>
-        </Grid2>
+          </Grid>
+        </Grid>
       </form>
+      {foodData && (
+        <Grid container justifyContent={"center"}>
+          <Grid item sm={8} sx={{boxShadow: 6, margin: 4, padding: 2, textAlign: "center"}}>
+            <Typography>{foodData.hits[0].recipe.label}</Typography>
+            <img alt="food-photo" src={foodData.hits[0].recipe.image}/>
+            <Typography>{foodData.hits[0].recipe.ingredients[0].food}</Typography>
+            <Typography>{foodData.hits[0].recipe.ingredients[1].food}</Typography>
+            <Typography>{foodData.hits[0].recipe.ingredientLines[0]}</Typography>
+            <Typography>{foodData.hits[0].recipe.ingredientLines[1]}</Typography>
+            <Typography>{foodData.hits[0].recipe.url}</Typography>
+          </Grid>
+        </Grid>
+      )}
     </Container>
   );
 }
