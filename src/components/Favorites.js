35430d84dbf90dcc7df3d9ba99da7c630d91c155
vknanavati <vimin.nanavati@gmail.com>
2024-09-05 10:47:39 -0400

    styled component with Grid, Typography.

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,15 +1,28 @@
 import Grid from '@mui/material/Grid2';
 import {RecipeCard} from './RecipeCard'
+import {Container,Typography} from '@mui/material';
 
 export function Favorites ({favorites}) {
     return (
-        <Grid>
-            {favorites.map((recipe,index)=>(
-                <RecipeCard
-                    recipe={recipe}
-                />
-            ))}
+        <Container>
+            <Grid container justifyContent={"center"} direction={"column"} alignItems={"center"}>
+                <Grid>
+                    <Typography
+                        variant="h4"
+                        sx={{marginTop: 5, marginBottom:2}}
+                    >
+                        Favorite Recipes
+                    </Typography>
+                </Grid>
+                {favorites.map((recipe,index)=>(
+                    <Grid>
+                        <RecipeCard
+                            recipe={recipe}
+                        />
+                    </Grid>
+                ))}
 
-        </Grid>
+            </Grid>
+        </Container>
     )
 }
