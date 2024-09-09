4cdde8b951979895cd0111141826c63bcfec7895
vknanavati <vimin.nanavati@gmail.com>
2024-09-09 15:17:03 -0400

    mapped makeRecipe state variable, added makeRecipe prop to component, styled with MUI.

--- src/components/MakeRecipe.js
+++ src/components/MakeRecipe.js
@@ -1,5 +1,35 @@
-export function MakeRecipe() {
+import { RecipeCard } from "./RecipeCard";
+import Grid from '@mui/material/Grid2';
+import {Container, Typography} from '@mui/material';
+
+export function MakeRecipe({makeRecipe}) {
     return(
-        <></>
+        <Container>
+            <Grid
+                container
+                justifyContent={"center"}
+                direction={"column"}
+                alignItems={"center"}
+            >
+                <Grid>
+                    <Typography
+                        variant="h4"
+                        sx={{marginTop: 5, marginBottom: 2}}
+                    >
+                        Make Recipe
+                    </Typography>
+                </Grid>
+                {makeRecipe.map((recipe, index)=>(
+                    <Grid>
+                        <RecipeCard
+                            recipe={recipe}
+                        />
+                    </Grid>
+
+
+                ))}
+
+            </Grid>
+        </Container>
     )
 }
