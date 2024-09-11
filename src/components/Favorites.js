102ed65d1775fa2be990ea88477ee1f0d354acdc
vknanavati <vimin.nanavati@gmail.com>
2024-09-11 13:44:13 -0400

    added padding to Container.

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -4,7 +4,7 @@ import {Container,Typography} from '@mui/material';
 
 export function Favorites ({favorites, addFavorite}) {
     return (
-        <Container>
+        <Container sx={{paddingTop: '64px'}}>
             <Grid container justifyContent={"center"} direction={"column"} alignItems={"center"}>
                 <Grid>
                     <Typography
