c77d3c465a29ba2c432879144b276d7d89373d98
vknanavati <vimin.nanavati@gmail.com>
2024-09-23 19:05:48 -0400

    added conditional rendering to display add/remove notifications for favorites.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import {useState} from 'react'
-import {Button,Container,TextField, Typography, Alert} from '@mui/material';
+import {Button,Container,TextField, Typography, Alert, AlertTitle} from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import { RecipeCard } from './RecipeCard';
 
-export function Home({addFavorite, foodData, setFoodData, addMakeRecipe, alert, setAlert}) {
+export function Home({addFavorite, foodData, setFoodData, addMakeRecipe, alertFavorite, setAlertFavorite, alertRemove, setAlertRemove}) {
     console.log(process.env)
 
     const [foodType, setFoodType] = useState("")
@@ -46,10 +46,10 @@ export function Home({addFavorite, foodData, setFoodData, addMakeRecipe, alert, 
               </Button>
             </Grid>
           </Grid>
-          {alert && (
+          {alertFavorite && (
             <Alert
               severity="success"
-              onClose={() => setAlert(false)}
+              onClose={() => setAlertFavorite(false)}
               sx={{
                 position: "fixed",
                 top: "30px",// 20px from the top of the screen
@@ -59,7 +59,24 @@ export function Home({addFavorite, foodData, setFoodData, addMakeRecipe, alert, 
                 zIndex: 9999 // alert appears above other content
               }}
             >
+              <AlertTitle>Success</AlertTitle>
               Recipe added to Favorites!
+            </Alert>
+          )}
+          {alertRemove && (
+            <Alert
+              severity="warning"
+              onClose={() => setAlertFavorite(false)}
+              sx={{
+                position: "fixed",
+                top: "30px",// 20px from the top of the screen
+                left: "50%", //left edge of alert at half the viewport width
+                transform: "translateX(-50%)", // moves alert to the left at half the width of the alert box
+                width: "auto", // adjust width based on content
+                zIndex: 9999 // alert appears above other content
+              }}
+            >
+              Removed from Favorites
             </Alert>
           )}
         </form>
